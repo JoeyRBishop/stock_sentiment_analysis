68f60b2c23bf3ebccbc989f8eda6cf953198b347
JoeyRBishop <rjbishop@hotmail.com>
2022-03-01 18:47:45 +0000

    Added doc strings to the functions

--- Twitter_api_calls_v1.py
+++ Twitter_api_calls_v1.py
@@ -4,42 +4,99 @@ import yaml
 from datetime import datetime, timedelta
 
 
-
+###As the twitter tokens are a secret, these are saved locally 
 with open("twitter_config.yaml", "r") as ymlfile:
     config = yaml.load(ymlfile)
     
 bearer_token=config["twitter_tokens"]["Bearer_token"]
 
 
-def create_headers(bearer_token):
-    headers = {"Authorization": f"Bearer {bearer_token}"}
-    return headers
-
-def create_url(keyword, start_date, end_date, max_results = 10):
+def create_query(keyword, start_date, end_date, max_results) -> dict:
+    '''
+    Creates the params part in the GET request
     
-    search_url = "https://api.twitter.com/2/tweets/search/recent"#Change to the endpoint you want to collect data from
+    Parameters
+    ----------
+    keyword : str
+        The key word/words that you want to be included in your search
+    start_date : str
+        The start of the time period that you would like twitter to search
+        between. 
+        In the format "%Y-%m-%dT%H:%M:%S.%fZ" i.e. "2022-02-22T00:00:00.000Z"
+    end_date : str
+        See start date, but obviously the end of the time period
+    max_results : int
+        The maximum number of results that you would returned, I believe 10 is
+        lowest and 100 is the highest number you cna choose
 
-    #change params based on the endpoint you are using
-    query_params = {'query': keyword,
-                    'start_time': start_date,
-                    'end_time': end_date,
-                    'max_results': max_results,
-                    'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
-                    'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
-                    'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
-                    'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
-                    'next_token': {}}
-    return (search_url, query_params)
+    Returns
+    -------
+    query : dict
+        Creates the params part in the GET request
 
-def connect_to_endpoint(url, headers, params, next_token = None):
-    params['next_token'] = next_token   #params object received from create_url function
+    '''
+
+    query = {'query': keyword,
+             'start_time': start_date,
+             'end_time': end_date,
+             'max_results': max_results,
+             'expansions' : 'author_id,geo.place_id,in_reply_to_user_id',
+             'tweet.fields' : 'author_id,created_at,geo,id,in_reply_to_user_id,lang,public_metrics,referenced_tweets,reply_settings,source,text',
+             'user.fields': 'created_at,description,id,location,name,public_metrics,username,verified',
+             'place.fields': 'country,country_code,full_name,geo,id,name,place_type',
+             'next_token': {}}
+    return query
+
+def connect_to_endpoint(url, headers, params, next_token = None) -> dict:
+    '''
+    The GET request of the API reponse
+
+    Parameters
+    ----------
+    url : str
+        The URL end point that is conected too
+    headers : dict
+        The authorisation token {"Authorization": f"Bearer {bearer_token}"}
+    params : dict
+        The query payload that the API requests
+    next_token : str/None, optional
+        DESCRIPTION. The default is None. But can be used to scroll through 
+        the reponses if the API has more matches than the maximum number of
+        results returned
+
+    Returns
+    -------
+    response.json() : dict
+        The response resposne of the API
+
+    '''
+    params['next_token'] = next_token 
     response = requests.request("GET", url, headers = headers, params = params)
-    print("Endpoint Response Code: " + str(response.status_code))
     if response.status_code != 200:
+        print("Endpoint Response Code: " + str(response.status_code))
         raise Exception(response.status_code, response.text)
+    breakpoint()
     return response.json()
 
-def end_time_calculate(start_time,days_added):
+def end_time_calculate(start_time,days_added) -> str:
+    '''
+    Twitter needs the date in a certain format, hence we add an int number of
+    days to the start date to obtain the end date.
+    In the format "%Y-%m-%dT%H:%M:%S.%fZ" i.e. "2022-02-22T00:00:00.000Z"
+
+    Parameters
+    ----------
+    start_time : str
+        In the format "%Y-%m-%dT%H:%M:%S.%fZ" i.e. "2022-02-22T00:00:00.000Z"
+    days_added : int
+        The number of days added to the start date to obtain the end date.
+
+    Returns
+    -------
+    end_time : str
+        The end date in correct format (see start_time)        
+
+    '''
     date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
     start_time = datetime.strptime(start_time, date_format)
     end_time=start_time+timedelta(days=days_added)
@@ -48,27 +105,59 @@ def end_time_calculate(start_time,days_added):
 
 
 
-def main_api_get(start_time):
-    headers = create_headers(bearer_token)
+def main_api_get(start_time,bearer_token):
+    '''
+    
+
+    Parameters
+    ----------
+    start_time : TYPE
+        DESCRIPTION.
+    bearer_token : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    json_response : TYPE
+        DESCRIPTION.
+
+    '''
+    end_point = "https://api.twitter.com/2/tweets/search/recent"
+    headers =  {"Authorization": f"Bearer {bearer_token}"}
     keyword = "TSLA lang:en"
     max_results = 10
     
     end_time=end_time_calculate(start_time,1)
     
-    url = create_url(keyword, start_time,end_time, max_results)
-    json_response = connect_to_endpoint(url[0], headers, url[1])
+    params = create_query(keyword, start_time,end_time, max_results)
+    json_response = connect_to_endpoint(end_point, headers, params)
     return json_response
 
 
 def save_json(filename,dict_payload):
+    '''
+    
+
+    Parameters
+    ----------
+    filename : str
+        the filename of the json file that you wish to save
+    dict_payload : str
+        the dict/json object to be saved
+
+    Returns
+    -------
+    Saves a json file
+
+    '''
     with open(f'{filename}.json', 'w+') as f:
         json.dump(dict_payload, f)
 # with open('output.json', 'r') as f:
 #     data=json.load(f)    
 
-start_time = "2022-02-22T00:00:00.000Z"
-for i in range(5):
-    payload=main_api_get(start_time)
+start_time = "2022-02-24T00:00:00.000Z"
+for i in range(1):
+    payload=main_api_get(start_time,bearer_token)
     save_json(f"TSLA_{start_time[:10]}",payload)
     start_time=end_time_calculate(start_time, 1)
     
